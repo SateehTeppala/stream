b29dcb06eb1d198416421c5e7e6d2f3064c57a77
SateehTeppala <33682034+SateehTeppala@users.noreply.github.com>
2023-01-06 04:00:48 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,19 +1,8 @@
-import pandas as pd
-import streamlit as st
-import numpy as np
+from flask import Flask
 
-st.set_page_config(page_title="BOOM",page_icon=":bar_chart:",layout="wide")
-st.title(":bar_chart: BOOM")
+app = Flask(__name__)
 
 
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
-
-st.area_chart(chart_data)
-
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
-
-st.line_chart(chart_data)
+@app.route("/")
+def home():
+    return "<h2>Flask Vercel</h2>"
